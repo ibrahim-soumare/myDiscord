c8ac0b8eee49a3dfaf3ad42d8020bd4f3dbac6f2
Rheda <rheda.rifki@laplateforme.io>
2024-02-15 12:09:06 +0100

    Ajout de la table qui permet de stocker les utilisateurs

--- database.sql
+++ database.sql
@@ -1,2 +1,14 @@
 -- Création de la base de données
 CREATE DATABASE IF NOT EXISTS mydiscord;
+
+-- Utilisation de la base de données
+USE mydiscord;
+
+-- Table pour stocker les utilisateurs
+CREATE TABLE IF NOT EXISTS users (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
