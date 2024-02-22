507d1095f77ec2fe117715db2b574ea4932b70ac
Rheda <rheda.rifki@laplateforme.io>
2024-02-22 13:45:49 +0100

    Ajout de la table messages

--- database.sql
+++ database.sql
@@ -4,7 +4,7 @@ CREATE DATABASE IF NOT EXISTS mydiscord;
 -- Utilisation de la base de données
 USE mydiscord;
 
--- Table pour stocker les utilisateurs
+
 CREATE TABLE IF NOT EXISTS users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
@@ -13,7 +13,7 @@ CREATE TABLE IF NOT EXISTS users (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
--- Table pour stocker les serveurs
+
 CREATE TABLE IF NOT EXISTS servers (
     server_id INT AUTO_INCREMENT PRIMARY KEY,
     server_name VARCHAR(100) NOT NULL,
@@ -23,11 +23,21 @@ CREATE TABLE IF NOT EXISTS servers (
 );
 
 
--- Table pour stocker les canaux de discussion
+
 CREATE TABLE IF NOT EXISTS channels (
     channel_id INT AUTO_INCREMENT PRIMARY KEY,
     channel_name VARCHAR(100) NOT NULL,
     server_id INT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (server_id) REFERENCES servers(server_id)
+);
+
+CREATE TABLE IF NOT EXISTS messages (
+    message_id INT AUTO_INCREMENT PRIMARY KEY,
+    message_text TEXT NOT NULL,
+    user_id INT,
+    channel_id INT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (channel_id) REFERENCES channels(channel_id)
 );
