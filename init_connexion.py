f5dcc75aac7e30dc764cf4743c9a90988fdbf1bc
adam-messaoudi <adam.messaoudi@laplateforme.io>
2024-02-28 13:36:18 +0100

    2eme Fonction afficher_fenetre_inscritpion(), creation de la fenetre avec le titre et les dimensions

--- init_connexion.py
+++ init_connexion.py
@@ -1,5 +1,15 @@
+from logging import root
 import tkinter as tk
 
 # Fonction pour afficher un message dans la console
 def afficher_message():
     print("Message affiché")
+
+# Fonction pour afficher la fenêtre d'inscription
+def afficher_fenetre_inscription():
+    # Création de la fenêtre d'inscription
+    fenetre_inscription = tk.Toplevel(root)
+    fenetre_inscription.title("Inscription")
+    fenetre_inscription.geometry("400x300")  # Définition de la taille de la fenêtre
+
+   
