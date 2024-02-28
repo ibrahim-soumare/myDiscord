0b91097dcaa88c02c1124443d71baf7359a69602
adam-messaoudi <adam.messaoudi@laplateforme.io>
2024-02-28 13:40:47 +0100

    Creation de Labels et champs d'entree pour l'inscription

--- init_connexion.py
+++ init_connexion.py
@@ -13,4 +13,9 @@ def afficher_fenetre_inscription():
     fenetre_inscription.geometry("400x300")  # Définition de la taille de la fenêtre
 
     # Changer le fond en noir et l'écriture en blanc pour la fenêtre d'inscription
-    fenetre_inscription.configure(bg="black")
+    fenetre_inscription.configure(bg="black")  # Labels et champs d'entrée pour l'inscription
+
+    # Labels et champs d'entrée pour l'inscription
+    etiquette_prenom = tk.Label(fenetre_inscription, text="Prénom:", bg="black", fg="white")
+    etiquette_prenom.grid(row=0, column=0, padx=5, pady=5)
+
