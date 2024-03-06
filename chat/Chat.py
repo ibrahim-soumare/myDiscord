2e7d19539e0d553e44c37269e31358e9e19dc6dd
ibrahim-soumare <ibrahim.soumare@laplateforme.io>
2024-03-06 18:15:22 +0100

    fonction envoyer message

--- chat/Chat.py
+++ chat/Chat.py
@@ -1,2 +1,8 @@
 import tkinter as tk
 from tkinter import scrolledtext
+
+def envoyer_message(zone_messages, champ_message):
+    message = champ_message.get()
+    if message:
+        zone_messages.insert(tk.END, f"Vous: {message}\n")
+        champ_message.delete(0, tk.END)
